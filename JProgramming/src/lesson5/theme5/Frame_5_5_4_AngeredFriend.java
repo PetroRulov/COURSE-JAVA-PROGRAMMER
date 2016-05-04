320a7909f66cec6bf78c73d5343900dc6f1422bc
prulov <prulov.pr@gmail.com>
2016-05-04 13:47:13 +0300

    frame 5_5_4 executed

--- JProgramming/src/lesson5/theme5/Frame_5_5_4_AngeredFriend.java
+++ JProgramming/src/lesson5/theme5/Frame_5_5_4_AngeredFriend.java
@@ -8,7 +8,6 @@ import java.util.Random;
 
 public class Frame_5_5_4_AngeredFriend extends JPanel {
 
-    JLabel cL;
     int x = 300;
     int y = 300;
 
@@ -20,91 +19,30 @@ public class Frame_5_5_4_AngeredFriend extends JPanel {
         frame.setMinimumSize(new Dimension(700, 700));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        //this.addMouseMotionListener(new MouseAdapter()
-
-
         this.addMouseMotionListener(new MouseAdapter() {
 
             @Override
-            public void mouseClicked(MouseEvent e) {
-//                System.out.println("Click");
-//                System.out.println(e.getX() + " : " + e.getY()+ " ; " + a + " : " + b);
-//                if (Math.abs(e.getX() - a + 35) < 35 && Math.abs(e.getY() - b + 35) < 35) {
-//                    System.out.println("YOU WIN");
-//                    System.exit(0);
-                //}
+            public void mouseMoved(MouseEvent e) {
 
-            }
-
-
-            @Override
-            public void mouseMoved(MouseEvent e){
-
-                if(circleInFrame()){
-
-                    if(e.getX() + 10 <= x){
-                        x++;
-                        repaint();
-                    }
-                    if(e.getX() - 10 >= x){
-                        x--;
-                        repaint();
-                    }
-                    if(e.getY() + 10 <= y){
-                        y++;
-                        repaint();
-                    }
-                    if(e.getY() - 10 >= y){
-                        y--;
-                        repaint();
-                    }
+                if(e.getX() <= 350 && e.getY() <= 350){
+                    x = e.getX() + 25;
+                    y = e.getY() + 25;
+                    repaint();
+                }else if(e.getX() > 350 && e.getY() <= 350){
+                    x = e.getX() - 75;
+                    y = e.getY() + 25;
+                    repaint();
+                }else if(e.getX() <= 350 && e.getY() > 350) {
+                    x = e.getX() + 25;
+                    y = e.getY() - 75;
+                    repaint();
                 }else{
-                    if(e.getY() > y){
-                        y++;
-
-                        repaint();
-                    }else{
-                        y--;
-
-                        repaint();
-                    }
-                    if(e.getX() > x){
-                        x++;
-
-                        repaint();
-                    }else{
-                        x--;
-
-                        repaint();
-                    }
+                    x = e.getX() - 75;
+                    y = e.getY() - 75;
+                    repaint();
                 }
             }
-
-            private boolean circleInFrame(){
-
-                if(x >= 10 & x < 590 && y >= 10 && y < 590){
-                    return true;
-                }
-                return false;
-            }
-
         });
-
-
-
-
-
-//        {
-//
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
-//
-//                if(e.getX() >= 125 && e.getX() <= 375 && e.getY() >= 25 && e.getY() <= 275){
-//                    repaint();
-//                }
-//
-//            }
-//        });
 
         frame.getContentPane().add(this);
         frame.pack();
@@ -116,14 +54,12 @@ public class Frame_5_5_4_AngeredFriend extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(new Color(255, 0, 0));
-        g.fillOval(x, y, 50, 50);
+        g.fillOval(x, y, 75, 75);
         g2d.setColor(new Color(255, 255, 255));
         g.setFont(new Font("Garamond", Font.BOLD, 10));
-        g.drawString("Click me!", x + 5, y + 30);
-
+        g.drawString("Click me!", x + 17, y + 45);
     }
 
-    // point of entrance
     public static void main(String[] args){
 
         new Frame_5_5_4_AngeredFriend();
