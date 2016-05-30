b86e737ccd422c57a45cdf6dc48fac5470dbe441
prulov <prulov.pr@gmail.com>
2016-05-30 06:32:36 +0300

    with SplashScreen Loading and Beer!

--- Gesheft/src/GESHLauncher.java
+++ Gesheft/src/GESHLauncher.java
@@ -1,4 +1,6 @@
-import java.awt.*;
+import guis.GeshGUI;
+import guis.Splash;
+import util.Service;
 
 /**
  * Created by prulov on 22.03.2016.
@@ -8,28 +10,14 @@ public class GeshLauncher {
 
     public static void main(String[] args)throws InterruptedException {
 
-        SplashScreen splash = SplashScreen.getSplashScreen();
-        Thread.sleep(2000);
+        new Splash();
 
-        Graphics2D g = splash.createGraphics();
-        g.setColor(Color.GREEN);
-        g.setFont(new Font("Garamond", Font.ITALIC, 21));
-        g.drawString("LOADING...", 15, 260);
-        String s = "_";
-        for(int i = 0; i < 7; i++){
-            g.drawString(s, 15, 265);
-            s+=s;
-            splash.update();
-            Thread.sleep(1000);
-        }
-        splash.close();
+//        Demo d = new Demo();
+//        d.openBadiganRun();
 
-        Demo d = new Demo();
-        d.openBadiganRun();
-
-//        Service serv = new Service();
-//        GeshGUI gGui = new GeshGUI(serv);
-//        gGui.setBadigan(serv.getBad());
+        Service serv = new Service();
+        GeshGUI gGui = new GeshGUI(serv);
+        gGui.setBadigan(serv.getBad());
 
     }
 }
